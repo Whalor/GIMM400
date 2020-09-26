84f0d5170eeaa5bac62d9fae90fc491a2ed1dad6
Whalor <54701426+Whalor@users.noreply.github.com>
2020-09-26 01:54:10 -0600

    Tank model
    The tank model is now linked with the code!

--- Tanks State Machine/Assets/Scripts/MouseCam.cs
+++ Tanks State Machine/Assets/Scripts/MouseCam.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class MouseCam : MonoBehaviour
 {
     public GameObject target;
-    public GameObject cannonHead;
     public float rotateSpeed = 5;
     Vector3 offset;
 
@@ -19,14 +18,12 @@ public class MouseCam : MonoBehaviour
     {
         //This find the horizontal speed
         float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
-        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
         target.transform.Rotate(0, horizontal, 0);
-        cannonHead.transform.Rotate(-vertical, 0, 0);
 
         //This creates a good angle for the viewport
         float desiredAngle = target.transform.eulerAngles.y;
         Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
-        transform.position = target.transform.position - (rotation * offset);
+        transform.position = target.transform.position - (rotation * offset * 1.5f);
 
         transform.LookAt(target.transform);
     }
